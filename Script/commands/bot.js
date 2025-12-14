63c4bc0a682375967d7c9a51629e45a7bf39639b
Hasib <kingtopvairal@gmail.com>
2025-12-14 05:09:10 +0300

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -195,7 +195,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("হুম ঝাং আমি তোমাকে রাইতে ভলোপাসি <🥵", threadID);
    };
 
-   if ((event.body.toLowerCase() == "ami Hasib") || (event.body.toLowerCase() == "kire")) {
+   if ((event.body.toLowerCase() == "ami Hasib") || (event.body.toLowerCase() == "আমি হাসিব")) {
      return api.sendMessage("হ্যা বস কেমন আছেন..?☺️", threadID);
    };
   mess = "{name}"
